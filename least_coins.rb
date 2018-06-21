96bf78507c373447c3795f1b4ff8241d80b2d3a0
Reaganhatt <reagan.hatt@gmail.com>
2018-06-21 16:08:33 +0000

    automatically backed up by learn

--- least_coins.rb
+++ least_coins.rb
@@ -1,7 +1,14 @@
 #write out your code here
 
 def least_coins(cents)
-
-#Code your answer here!
-
+  {
+  :quarters => 25, 
+  :dimes => 10, 
+  :nickels => 5, 
+  :pennies => 1
+}
+  least_coins.each do |x|
+    least_coins(19)
+    puts least_coins
+  end
 end
